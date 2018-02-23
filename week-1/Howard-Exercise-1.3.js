fac9c921f3bb9ac484940a2e2371f1808dc57b70
Matthew Howard <mhoward14@icloud.com>
2018-02-22 21:17:38 -0700

    Adding week-1/Howard-Exercise-1.3.js

--- week-1/Howard-Exercise-1.3.js
+++ week-1/Howard-Exercise-1.3.js
@@ -1,3 +1,17 @@
+var header = require('../header.js');
+var headerMsg = header.display("Matthew", "Howard", "Exercise 1.3");
+console.log(headerMsg);
+
+/*
+ Expected output:
+
+ Matthew Howard
+ Exercise 1.3
+ 22 February 2018
+
+ */
+ //start program 
+
 var url = require('url');
 
 var parsedURL = url.parse('https://www.example.com/profile?name=howard')
@@ -5,3 +19,5 @@ var parsedURL = url.parse('https://www.example.com/profile?name=howard')
 console.log(parsedURL.protocol);
 console.log(parsedURL.host);
 console.log(parsedURL.query);
+
+//end program
