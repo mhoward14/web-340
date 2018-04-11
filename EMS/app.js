8fc1aa1c23a01dd91020198c4661bb672734d941
Matthew Howard <mhoward14@icloud.com>
2018-04-10 19:37:51 -0600

    Adding app.js

--- EMS/app.js
+++ EMS/app.js
@@ -3,17 +3,23 @@ Matthew Howard
 EMS (Milestone 1): User Interface Development
 22 March 2018
 Updated: 7 April 2018
+Updated: 10 April 2018
 */
 //start program
-var express = require("express");
 var http = require("http");
 var path = require("path");
-var mongoose = require("mongoose");
+var csrf = require("csurf");
 var logger = require("morgan");
 var helmet = require("helmet");
+var express = require("express");
+var mongoose = require("mongoose");
+var bodyParser = require("body-parser");
 var Employee = require("./models/employee");
+var cookieParser = require("cookie-parser");
 
 var app = express();
+
+var csrfProtection = csrf({cookie: true});
 
 var mongoDB = "mongodb://mhoward007:R3dsRUnit3d@ds117469.mlab.com:17469/ems";
 mongoose.connect(mongoDB, {
@@ -35,11 +41,29 @@ app.set("views", path.resolve(__dirname, "views"));
 app.set("view engine", "ejs");
 app.use(logger("short"));
 app.use(helmet.xssFilter());
-app.get("/", function (req, res) {
-    res.render("index", {
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+app.use(cookieParser());
+app.use(csrfProtection);
+app.use(function(request, response, next){
+    var token = request.csrfToken();
+    response.cookie('XSRF-TOKEN', token);
+    response.locals.csrfToken = token;
+    next();
+});
+
+app.get("/", function (request, response) {
+    response.render("index", {
         title: "Home page",
         message: "XSS Prevention Example"
     });
+});
+
+app.post("/process", function (request, response){
+    console.log(request.body.txtName);
+    response.redirect("/");
 });
 
 http.createServer(app).listen(8080, function(){
